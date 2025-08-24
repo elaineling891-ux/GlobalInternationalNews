6f13f2ce0c69246c2af3265b85c7766b4811528f
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-24 11:34:02 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-import sqlite3
 
 DB_URL = os.getenv("DATABASE_URL")
 
@@ -64,7 +63,7 @@ def get_news(limit=20, offset=0):
     ]
 
 def get_all_news(skip=0, limit=20):
-    conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute(
